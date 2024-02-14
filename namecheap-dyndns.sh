9925472d18227ccdfa6987b1b16d1367f7396c55
Krisztián Villers <117743062+notvillers@users.noreply.github.com>
2024-02-14 11:05:12 +0100

    updt

--- namecheap-dyndns.sh
+++ namecheap-dyndns.sh
@@ -1,42 +1,47 @@
 #!/bin/bash
-# arg1: domain_name
-# arg2: ddns_password
+
+log_message() {
+    log_file="log.txt"
+    current_datetime=$(date +"%Y-%m-%d %H:%M:%S")
+    echo "[$current_datetime] $1"
+    echo "[$current_datetime] $1" >> "$log_file"
+}
 
 if [ "$#" -ne 2 ]; then
-    echo "usage: $0 domain_name ddns_password"
+    echo "usage: $0 arg1 arg2"
     exit 1
 fi
 
 script_dir=$(dirname "$0")
 cd $script_dir
+log_message "moved to ${script_dir}"
 
 ipv4=$(curl -s "http://ipv4.icanhazip.com")
-echo "ipv4: ${ipv4}"
+log_message "ipv4: ${ipv4}"
 
-logfile=$"ipv4.log"
+ipv4_log="ipv4.log"
 
-if [ ! -e "${logfile}" ]; then
-    touch "${logfile}"
-    echo "${logfile} created"
-    echo "${ipv4}" > $logfile
+if [ ! -e "${ipv4_log}" ]; then
+    touch "${ipv4_log}"
+    log_message "${ipv4_log} created"
+    echo "${ipv4}" > $ipv4_log
+    log_message "${ipv4} stored to ${ipv4_log}"
 else
-    echo "$logfile found"
-    read -r stored_ipv4 < $logfile
-    echo "${stored_ipv4}"
+    log_message "$ipv4_log found"
+    read -r stored_ipv4 < $ipv4_log
+    log_message "stored: ${stored_ipv4}"
 fi
 
 if [ "${ipv4}" != "${stored_ipv4}" ]; then
-    echo "ipv4 address changed"
-    echo "${ipv4}" > $logfile
-    echo "new ipv4 address stored"
+    log_message "ipv4 address changed"
+    echo "${ipv4}" > $ipv4_log
+    log_message "new ipv4 address stored"
     host="@"
     domain_name="${1}"
     ddns_password="${2}"
     your_ip="${ipv4}"
     requesthttps=$"https://dynamicdns.park-your-domain.com/update?host=${host}&domain=${domain_name}&password=${ddns_password}&ip=${ipv4}"
-    echo "${requesthttps}"
     resquestresult=$(curl "${requesthttps}")
-    echo "${resquestresult}"
 else
-    echo "ipv4 address did not change"
+    log_message "ipv4 address did not change"
 fi
--- start.sh
+++ start.sh
@@ -2,11 +2,7 @@
 cd $(dirname "$0")
 
 datafile="data.txt"
+domain_name=$(head -n 1 "$datafile")
+ddns_password=$(head -n 2 "$datafile" | tail -n 1)
 
-first_line=$(head -n 1 "$datafile")
-echo "First line: $first_line"
-
-second_line=$(head -n 2 "$datafile" | tail -n 1)
-echo "Second line: $second_line"
-
-bash namecheap-dyndns.sh $first_line $second_line
+bash namecheap-dyndns.sh $domain_name $ddns_password
